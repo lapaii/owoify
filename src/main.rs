13d529de5f6f9b49d99085b27fb5e8327f7378a1
lapaii <lapaii@lapaii.dev>
2024-06-22 23:16:11 +0100

    help and version pages + 0.2.0

--- src/main.rs
+++ src/main.rs
@@ -37,10 +37,24 @@ fn main() {
         1 onwards -> text to be owo-fied
     */
     let mut args: Vec<String> = env::args().collect();
+    let version = env!("CARGO_PKG_VERSION");
 
     args.remove(0); // removes the location of the binary from args
 
-    let mut input = args.join(" ").to_ascii_lowercase(); // joins the remaining args into one string and forces lowercase
+    if args.is_empty() || args[0].to_lowercase() == "--h" || args[0].to_lowercase() == "--help" {
+        print!(
+            "usage: owoify <input text>\n\noptions:\n\n--h --help print help\n--v --version print version\n\nversion: {}\n",
+            version
+        );
+        std::process::exit(0);
+    }
+
+    if args[0].to_lowercase() == "--version" || args[0].to_lowercase() == "--v" {
+        println!("version: {}", version);
+        std::process::exit(0);
+    }
+
+    let mut input = args.join(" ").to_lowercase(); // joins the remaining args into one string and forces lowercase
 
     for (pattern, replacement) in &dict {
         input = input.replace(pattern, replacement); // actually replace the string
